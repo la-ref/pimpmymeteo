1d6b5028f649c6ee7f41567189eb68b20b049ef3
la-ref <jmermoud81@gmail.com>
2022-10-14 11:12:31 +0200

    récupération de la tempérture dans le json et mise à jour du texte

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    $("#").click(function(e) {
+    $("#buttonTemp").click(function(e) {
         e.preventDefault();
+        var ville = $("#villeInput").val();
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=calais&appid=32fc6204bf134f2a5be8140ff0e93cdf&units=metric",
+            url: "https://api.openweathermap.org/data/2.5/weather?q="+ville +"&appid=32fc6204bf134f2a5be8140ff0e93cdf&units=metric",
             type: 'GET',
             dataType: 'json',
             success: function(res) {
-                console.log(res);
-                alert(res);
+                $("#resultat").html(Math.round(res.main.temp));
             }
         });
     });
