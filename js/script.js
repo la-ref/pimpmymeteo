a19971685556ddbafde9bff632a71dd27f454cdf
la-ref <jmermoud81@gmail.com>
2022-10-14 11:02:19 +0200

    récupération de la réponse de l'api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+    $("#").click(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q=calais&appid=32fc6204bf134f2a5be8140ff0e93cdf&units=metric",
+            type: 'GET',
+            dataType: 'json',
+            success: function(res) {
+                console.log(res);
+                alert(res);
+            }
+        });
+    });
 });
